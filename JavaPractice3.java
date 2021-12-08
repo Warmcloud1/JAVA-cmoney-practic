6420a2d26d478bf2e27efdaa6244dc64581ab48e
KuanLin <36700601+kavinqq@users.noreply.github.com>
2021-12-08 21:37:42 +0800

    Create JavaPractice3.java

---
+++ JavaPractice3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class JavaPractice3{
+    public static void main(String[] argv){        
+        Scanner sc=new Scanner(System.in);
+        int i;
+        int[] num=new int[3];
+        for(i=0;i<3;i++){
+            num[i]=sc.nextInt();
+        }           
+        
+        i=i-1;
+        int min=num[i];
+        
+        while(i>=1){
+            if(min>num[i-1])
+                min=num[i-1];
+            i--;
+        }
+        
+        System.out.print(min);
+    }
+}
