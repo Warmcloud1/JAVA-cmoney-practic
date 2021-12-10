d8b81dc8d65fcd50b7303d07d37a8921333c473f
KuanLin <36700601+kavinqq@users.noreply.github.com>
2021-12-10 19:46:06 +0800

    Create IsPrime.java

---
+++ IsPrime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Prime{
+  void isprime(int n){
+    boolean prime=true;
+  	for(int i=2;i<=n/2;i++){
+      if(n%i==0){
+        prime=false;
+        break;
+      }
+    }
+    if(prime) System.out.println("true");
+    else System.out.println("false");
+  }
+}
+      
+    
+public class IsPrime {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Prime p=new Prime();
+    int i=0,times;
+    times=sc.nextInt();
+    int[] arr_num=new int[times];
+    while(times-->0)
+      arr_num[i++]=sc.nextInt();
+    for(Integer j:arr_num)
+      p.isprime(j);
+  }
+}
